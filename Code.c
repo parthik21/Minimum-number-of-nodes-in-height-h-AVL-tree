f01cd0ce5f1e620bedc331c1f08b10ebd83439cc
Parthik Rudakiya <59130437+parthik21@users.noreply.github.com>
2021-05-08 16:16:53 +0530

    Update Code.c

--- Code.c
+++ Code.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 int avl(int h){
-    if(!h)
+    if(!h)                     // check whether input is correct?
         return 0;
-    else if(h==1)
+    else if(h==1)              // base condition
         return 2;
-    else if(h==2)
+    else if(h==2)              // base condition
         return 4;
     else 
-        return 1+avl(h-1)+avl(h-2);
+        return 1+avl(h-1)+avl(h-2);     // Recursion
 }
 
 int main() {
@@ -16,8 +16,8 @@ int main() {
     int h;
     
     printf("Enter height of AVL : ");
-    scanf("%d",&h);
-    printf("Minimum number of nodes in AVL of height=%d is %d.",h,avl(h));
+    scanf("%d",&h);                                                            // Input taken from user
+    printf("Minimum number of nodes in AVL of height=%d is %d.",h,avl(h));     // Output number
     
     return 0;
 }
